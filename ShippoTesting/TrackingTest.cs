75802ea4038d2a740b894f2b1a3ff6ab9e2f5329
Vinh Vu <kroomey@gmail.com>
2017-02-11 18:09:52 -0800

    Add Tracking test file with inital get status test

---
+++ ShippoTesting/TrackingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Shippo;
+
+namespace ShippoTesting
+{
+    [TestFixture ()]
+    public class TrackingTest : ShippoTest
+    {
+        [Test ()]
+        public void TestValidGetStatus ()
+        {
+            Shipment shipment = ShipmentTest.getDefaultObject ();
+            Tracking tracking = getAPIResource ().RetrieveTracking ("usps", shipment.ObjectId);
+            Assert.IsNotNull (tracking.TrackingNumber);
+            Assert.IsNotNull (tracking.TrackingHistory);
+        }
+    }
+}
