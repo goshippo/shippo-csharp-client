e95152e3751f723511118a76a750d40d0966ed7c
Vinh Vu <kroomey@gmail.com>
2017-02-11 18:16:46 -0800

    Add tests for registering tracking webhooks

--- ShippoTesting/TrackingTest.cs
+++ ShippoTesting/TrackingTest.cs
@@ -27,5 +27,31 @@ namespace ShippoTesting
         {
             getAPIResource ().RetrieveTracking ("usps", "INVALID_ID");
         }
+
+        [Test ()]
+        public void TestValidRegisterWebhook ()
+        {
+            Shipment shipment = ShipmentTest.getDefaultObject ();
+            Tracking tracking = getAPIResource ().RetrieveTracking ("usps", shipment.ObjectId);
+            Assert.IsNotNull (tracking.TrackingNumber);
+            Assert.IsNotNull (tracking.TrackingHistory);
+
+            Hashtable parameters = new Hashtable ();
+            parameters.Add ("carrier", "usps");
+            parameters.Add ("tracking_number", tracking.TrackingNumber);
+            Tracking register = getAPIResource ().RegisterTrackingWebhook (parameters);
+            Assert.IsNotNull (register.TrackingNumber);
+            Assert.IsNotNull (register.TrackingHistory);
+        }
+
+        [Test ()]
+        [ExpectedException (typeof (ShippoException))]
+        public void TestInvalidRegisterWebhook ()
+        {
+            Hashtable parameters = new Hashtable ();
+            parameters.Add ("carrier", "usps");
+            parameters.Add ("tracking_number", "INVALID_ID");
+            getAPIResource ().RegisterTrackingWebhook (parameters);
+        }
     }
 }
