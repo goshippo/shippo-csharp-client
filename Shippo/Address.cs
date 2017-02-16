08a71b4c6ffadfa711be63a5caa2e95cfa8ef498
Vinh Vu <kroomey@gmail.com>
2017-02-16 12:15:22 -0800

    Clarify Address property types and removed getters/setters

--- Shippo/Address.cs
+++ Shippo/Address.cs
@@ -1,67 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Shippo {
     [JsonObject (MemberSerialization.OptIn)]
     public class Address : ShippoId {
         [JsonProperty (PropertyName = "object_state")]
-        public object ObjectState { get; set; }
+        public string ObjectState;
 
         [JsonProperty (PropertyName = "object_purpose")]
-        public object ObjectPurpose { get; set; }
+        public string ObjectPurpose;
 
         [JsonProperty (PropertyName = "object_source")]
-        public object ObjectSource { get; set; }
+        public string ObjectSource;
 
         [JsonProperty (PropertyName = "object_created")]
-        public object ObjectCreated { get; set; }
+        public DateTime? ObjectCreated;
 
         [JsonProperty (PropertyName = "object_updated")]
-        public object ObjectUpdated { get; set; }
+        public DateTime? ObjectUpdated;
 
         [JsonProperty (PropertyName = "object_owner")]
-        public object ObjectOwner { get; set; }
+        public string ObjectOwner;
 
         [JsonProperty (PropertyName = "name")]
-        public object Name { get; set; }
+        public string Name;
 
         [JsonProperty (PropertyName = "company")]
-        public object Company { get; set; }
+        public string Company;
 
         [JsonProperty (PropertyName = "street1")]
-        public object Street1 { get; set; }
+        public string Street1;
 
         [JsonProperty (PropertyName = "street_no")]
-        public object StreetNo { get; set; }
+        public string StreetNo;
 
         [JsonProperty (PropertyName = "street2")]
-        public object Street2 { get; set; }
+        public string Street2;
+
+        [JsonProperty (PropertyName = "street3")]
+        public string Street3;
 
         [JsonProperty (PropertyName = "city")]
-        public object City { get; set; }
+        public string City;
 
         [JsonProperty (PropertyName = "state")]
-        public object State { get; set; }
+        public string State;
 
         [JsonProperty (PropertyName = "zip")]
-        public object Zip { get; set; }
+        public string Zip;
 
         [JsonProperty (PropertyName = "country")]
-        public object Country { get; set; }
+        public string Country;
 
         [JsonProperty (PropertyName = "phone")]
-        public object Phone { get; set; }
+        public string Phone;
 
         [JsonProperty (PropertyName = "email")]
-        public object Email { get; set; }
+        public string Email;
 
-        [JsonProperty (PropertyName = "ip")]
-        public object IP { get; set; }
+        [JsonProperty (PropertyName = "is_residential")]
+        public bool? IsResidential;
+
+        [JsonProperty (PropertyName = "validate")]
+        public bool? Validate;
 
         [JsonProperty (PropertyName = "metadata")]
-        public object Metadata { get; set; }
+        public string Metadata;
 
-	[JsonProperty (PropertyName = "is_residential")]
-	public object IsResidential { get; set; }
+        [JsonProperty (PropertyName = "test")]
+        public bool Test;
+
+        [JsonProperty (PropertyName = "messages")]
+        public List<string> Messages;
     }
 }
