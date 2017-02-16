ad8364fdfe404404173a2f714af5a694a42453f2
Vinh Vu <kroomey@gmail.com>
2017-02-16 15:50:45 -0800

    Move Batch specific Address class to Batch

--- Shippo/Batch.cs
+++ Shippo/Batch.cs
@@ -38,6 +38,59 @@ namespace Shippo {
         [JsonProperty (PropertyName = "object_results")]
         public ObjectResults ObjectResults;
 
+        public class Address
+        {
+            [JsonProperty (PropertyName = "object_purpose")]
+            public string ObjectPurpose;
+
+            [JsonProperty (PropertyName = "name")]
+            public string Name;
+
+            [JsonProperty (PropertyName = "company")]
+            public string Company;
+
+            [JsonProperty (PropertyName = "street1")]
+            public string Street1;
+
+            [JsonProperty (PropertyName = "street2")]
+            public string Street2;
+
+            [JsonProperty (PropertyName = "city")]
+            public string City;
+
+            [JsonProperty (PropertyName = "state")]
+            public string State;
+
+            [JsonProperty (PropertyName = "zip")]
+            public string Zip;
+
+            [JsonProperty (PropertyName = "country")]
+            public string Country;
+
+            [JsonProperty (PropertyName = "phone")]
+            public string Phone;
+
+            [JsonProperty (PropertyName = "email")]
+            public string Email;
+
+            public static Address createForPurchase (String purpose, String name, String street1, String street2, String city,
+                                                     String state, String zip, String country, String phone, String email)
+            {
+                Address a = new Address ();
+                a.ObjectPurpose = purpose;
+                a.Name = name;
+                a.Street1 = street1;
+                a.Street2 = street2;
+                a.City = city;
+                a.State = state;
+                a.Zip = zip;
+                a.Country = country;
+                a.Phone = phone;
+                a.Email = email;
+                return a;
+            }
+        }
+
         public class BatchShipment
         {
             [JsonProperty (PropertyName = "shipment")]
