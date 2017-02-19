1eab2cfb2dc6e732190811b5ecbc58edbac30609
Vinh Vu <kroomey@gmail.com>
2017-02-19 15:17:26 -0800

    Add enums class

---
+++ Shippo/ShippoEnums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Shippo
+{
+    public static class ShippoEnums
+    {
+        public enum LabelFiletypes { PNG, PDF, PDF_4X6, ZPLII }
+        public enum ObjectStatuses { VALIDATING, VALID, INVALID, PURCHASING, PURCHASED }
+        public enum ObjectResults { CREATION_FAILED, CREATION_SUCCEEDED, PURCHASE_FAILED, PURCHASE_SUCCEEDED }
+    }
+}
