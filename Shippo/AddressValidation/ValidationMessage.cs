58987617a429f748e7d06faf2434130f5a0c0b64
Vinh Vu <vinh@goshippo.com>
2017-07-18 10:25:51 -0700

    Add Address Validation objects

---
+++ Shippo/AddressValidation/ValidationMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Shippo {
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ValidationMessage : ShippoId {
+        [JsonProperty(PropertyName = "source")]
+        public String Source { get; set; }
+
+        [JsonProperty(PropertyName = "code")]
+        public String Code { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public String Type { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public String Text { get; set; }
+    }
+}
