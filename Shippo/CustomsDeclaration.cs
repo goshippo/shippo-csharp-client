2159defda7aa0d3574b3b9b46585e5c6115f0b65
Wei Wen <wei@goshippo.com>
2017-10-07 16:55:25 -0700

    add address_importer field

--- Shippo/CustomsDeclaration.cs
+++ Shippo/CustomsDeclaration.cs
@@ -55,6 +55,9 @@ namespace Shippo {
         [JsonProperty(PropertyName = "certify_signer")]
         public object CertifySigner { get; set; }
 
+        [JsonProperty (PropertyName = "address_importer")]
+        public object AddressImporter { get; set; }
+
         [JsonProperty(PropertyName = "disclaimer")]
         public object Discliamer { get; set; }
 
