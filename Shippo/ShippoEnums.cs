87c0543de2d82a52bc5e22c83023f9e37eaefe88
AJ Hoffer <453104+dochoffiday@users.noreply.github.com>
2019-02-12 14:12:21 -0500

    add `PRE_TRANSIT` to `TrackingStatus` enum

--- Shippo/ShippoEnums.cs
+++ Shippo/ShippoEnums.cs
@@ -6,6 +6,6 @@ namespace Shippo
         public enum LabelFiletypes { NONE, PNG, PDF, PDF_4x6, ZPLII }
         public enum Statuses { VALIDATING, VALID, INVALID, PURCHASING, PURCHASED }
         public enum ObjectResults { none, creation_failed, creation_succeeded, purchase_failed, purchase_succeeded }
-        public enum TrackingStatus { UNKNOWN, DELIVERED, TRANSIT, FAILURE, RETURNED }
+        public enum TrackingStatus { UNKNOWN, DELIVERED, TRANSIT, FAILURE, RETURNED, PRE_TRANSIT }
     }
 }
