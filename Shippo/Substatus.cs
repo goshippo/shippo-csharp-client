a00025f3b672ae216cdb1684ea70ecf91498f7da
AJ Hoffer <aj@hoffer.fm>
2020-12-22 12:06:30 -0500

    add substatus to tracking status

---
+++ Shippo/Substatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Shippo
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Substatus
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code;
+
+        [JsonProperty(PropertyName = "text")]
+        public string Test;
+
+        [JsonProperty(PropertyName = "action_required")]
+        public bool ActionRequired;
+    }
+}
