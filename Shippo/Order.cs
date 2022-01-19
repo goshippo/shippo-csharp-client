4122cb9bc6bddb27eecd0181abc40f3531f236ac
ZarlengoToptal <85881377+ZarlengoToptal@users.noreply.github.com>
2022-01-19 14:38:40 -0800

    Add orders and pickups (#72)
    * Add pickups and orders
    
    * correct typo
    
    * version change
    
    * Add missing line items
    
    * Fix generic & hidden member
    
    Co-authored-by: Chris <chris_eng@goshippo.com>

---
+++ Shippo/Order.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Shippo {
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Order : ShippoId {
+        [JsonProperty(PropertyName = "object_owner")]
+        public object ObjectOwner { get; set; }
+
+        [JsonProperty(PropertyName = "order_number")]
+        public object OrderNumber { get; set; }
+
+        [JsonProperty(PropertyName = "order_status")]
+        public object OrderStatus { get; set; }
+
+        [JsonProperty(PropertyName = "placed_at")]
+        public object PlacedAt { get; set; }
+
+        [JsonProperty(PropertyName = "to_address")]
+        public object ToAddress { get; set; }
+
+        [JsonProperty(PropertyName = "from_address")]
+        public object FromAddress { get; set; }
+
+        [JsonProperty(PropertyName = "line_items")]
+        public object LineItems { get; set; }
+
+        [JsonProperty(PropertyName = "shipping_cost")]
+        public object ShippingCost { get; set; }
+
+        [JsonProperty(PropertyName = "shipping_cost_currency")]
+        public object ShippingCostCurrency { get; set; }
+
+        [JsonProperty(PropertyName = "shipping_method")]
+        public object ShippingMethod { get; set; }
+
+        [JsonProperty(PropertyName = "shop_app")]
+        public object ShopApp { get; set; }
+
+        [JsonProperty(PropertyName = "subtotal_price")]
+        public object SubtotalPrice { get; set; }
+
+        [JsonProperty(PropertyName = "total_price")]
+        public object TotalPrice { get; set; }
+
+        [JsonProperty(PropertyName = "total_tax")]
+        public object TotalTax { get; set; }
+
+        [JsonProperty(PropertyName = "currency")]
+        public object Currency { get; set; }
+
+        [JsonProperty(PropertyName = "transactions")]
+        public object Transactions { get; set; }
+
+        [JsonProperty(PropertyName = "weight")]
+        public object Weight { get; set; }
+
+        [JsonProperty(PropertyName = "weight_unit")]
+        public object WeightUnit { get; set; }
+
+        [JsonProperty(PropertyName = "notes")]
+        public object Notes { get; set; }
+
+        [JsonProperty(PropertyName = "test")]
+        public object Test { get; set; }
+
+        [JsonProperty(PropertyName = "metadata")]
+        public object Metadata { get; set; }
+    }
+}
+
---
+++ ShippoTesting/PickupTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+
+using Shippo;
+
+
+namespace ShippoTesting {
+    [TestFixture]
+    public class PickupTest : ShippoTest {
+    /* Intentionally commented; cannot be tested without proper billing credentials
+        
+        [Test]
+        public void TestValidCreate()
+        {
+            Hashtable transactionParameters = new Hashtable();
+            Shipment shipment = ShipmentTest.getDefaultObject();
+            var rate = shipment.Rates[0];
+
+            transactionParameters.Add("rate", rate.ObjectId);
+            transactionParameters.Add("async", false);
+            transactionParameters.Add("label_file_type", "PDF");
+            Transaction transaction = getAPIResource().CreateTransactionSync(transactionParameters);
+            
+            Hashtable location = new Hashtable();
+            location.Add("building_location_type", "Knock on Door");
+            location.Add("address", shipment.AddressTo);
+
+            var pickupStartTime = DateTime.UtcNow;
+            var pickupEndTime = pickupStartTime.AddDays(1);
+
+            Hashtable parameters = new Hashtable();
+            parameters.Add("carrier_account", rate.ObjectId);
+            parameters.Add("location", location);
+            parameters.Add("transactions", new String[]{ transaction.ObjectId });
+            parameters.Add("requested_start_time", pickupStartTime.ToString("o"));
+            parameters.Add("requested_end_time", pickupEndTime.ToString("o"));
+            parameters.Add("is_test", false);
+
+            Pickup testObject = getAPIResource().CreatePickup(parameters);
+            Assert.AreEqual("SUCCESS", testObject.Status);
+        }*/
+    }
+}
